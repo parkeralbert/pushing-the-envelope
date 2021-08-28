bf572249c539f800cca6df76a3464cccf618a23a
parkeralbert <74433019+parkeralbert@users.noreply.github.com>
2021-08-28 16:55:07 -0400

    first update

--- src/lists/models/constants.ts
+++ src/lists/models/constants.ts
@@ -35,6 +35,127 @@ export const MOCK_LISTS: ReadonlyArray<List> = [
         content: `<b>Best album to get you into meditation.</b>
         <p>The most basic premise of meditation is to sit and be present. The breath is often used as a focal point - if one gets distracted by thoughts or outside stimuli, focus can always be turned back towards this life force. Phil Elverium&rsquo;s <em>Microphones in 2020</em> starts with just the breath. For the first 8 minutes of the song&rsquo;s forty-five minute duration, two chords played on a five dollar acoustic guitar glide and cascade, unperturbed and at peace. Once Phil enters, he beginsan exercise in excavation, examining his life at three different ages: 17, 20, and 23. Life's undercurrent carries him through ruminations on the frivolity of band names, the tangibility of flashbulb memories, and the strange career of making art, but the song always returns to the breath. <em>Microphones in 2020 </em>simultaneously explores the slippery paradox of the present and the ceaseless nature of time, expressed succinctly in the final lines: "If there have to be words, they could just be/now only and there's no end."</p>`,
       },
+      {
+        id: 'fiona-apple-fetch-the-bolt-cutters',
+        title: 'Fetch The Bolt Cutters',
+        artist: 'Fiona Apple',
+        content: `<b>Best album I can never listen to again</b>
+        <p>When Fiona casually released this masterpiece into the ether, Pitchfork immediately awarded the album with a perfect ten, something it hadn&rsquo;t done in a decade. Publications stumbled over each other to heap praise upon the undeniable power of <em>Fetch the Bolt Cutters</em>, while I myself was scouring over every song in an attempt to glean each stroke of genius. It&rsquo;s an impossibly active record that crashes you through the ceiling of Fiona&rsquo;s home turned explosive parade featuring clanging percussion found in schoolyards; it&rsquo;s a rapidfire joyride through the synapses of her brain. After a few months of residing here, I began to feel like I was overstaying my welcome, and had to take a long break from listening to the album. Nevertheless, I come by to visit every now and then, and it&rsquo;s always a pleasure.&nbsp;</p>`,
+      },
+      {
+        id: 'the-soft-pink-truth-shall-we-go-on-sinning-so-that-grace-may-increase',
+        title: 'Shall We Go On Sinning So That Grace May Increase?',
+        artist: 'The Soft Pink Truth',
+        content: `<b>Best album to listen to while locking yourself in a sauna and taking too many drugs</b>
+        <p>Drew Daniel&rsquo;s latest solo effort effectively captures what it would sound like if Steve Reich had been an avid raver. The opening track features angelic voices hypnotically repeating the record&rsquo;s title amidst chiming bells, imbuing the album with a strangely monastic quality. Fittingly, the two most important moments are found on the tracks &ldquo;Sinning'' and &ldquo;Grace.&rdquo; On the former, piano, clarinet, and saxophone circle one another in minimalist patterns, while booming bass and syncopated handclaps set up a sweaty club atmosphere. On &ldquo;Grace,&rdquo; a gentle piano melody meanders through swirling synths, dissipates into a cloud of raucous ambience, and slyly builds into the album&rsquo;s most transcendent moment. The perfect balance of soothing minimalism and adrenaline-pumping dance music, <em>Shall We Go On Sinning So That Grace May Increase? </em>maps out an ideal vision of ecstasy.</p>`,
+      },
+      {
+        id: 'jeff-parker-suite-for-max-brown',
+        title: 'Suite For Max Brown',
+        artist: 'Jeff Parker',
+        content: `<b>Best album to listen to when it’s raining outside</b>
+        <p>Jeff Parker&rsquo;s <em>Suite for Max Brown </em>is cut from the same exploratory cloth as his renowned work with post-rock group Tortoise. Evading sonic expectations, Parker relentlessly tinkers with the jazz template to incorporate spaced-out ambience and trance-like rhythms into a delightful mood board. The methodical mindset of interlocking grooves on "Fusion Swirl" evaporates into atmospheric track &ldquo;After The Rain,&rdquo; which drizzles drowsy guitar over Vangelis-textured keys, perfectly evoking its namesake. After taking its time on a winding path that keeps you guessing, the record rides out on the ten minute closer &ldquo;Max Brown.&rdquo; Perhaps the most traditional track on the album, it&rsquo;s a satisfying send-off that proves Parker can play it straight too.</p>`,
+      },
+      {
+        id: 'the-1975-notes-on-a-conditional-form',
+        title: 'Notes On a Conditional Form',
+        artist: 'The 1975',
+        content: `<b>Most consistently inconsistent album</b>
+        <p>Imagine a new thrift store opened up a few blocks from your place. You&rsquo;ve heard from friends it&rsquo;s a bit janky, but you still want to check it out. It&rsquo;s called <em>Notes On a Conditional Form</em>. Upon entering the store, you&rsquo;re immediately greeted by Greta Thurnburg, who delivers a rousing speech about the immediacy of addressing climate change over twinkling piano. <em>That&rsquo;s very inspiring,</em> you think, <em>but what a bizarre introduction. </em>As you shuffle past Greta and make your way down an aisle, you notice something that seems out of place. A cinematic interlude of Disney proportions (&ldquo;The End (Music for Cars)&rdquo;) and a slice of pastiche punk (&ldquo;People&rdquo;) sit side by side, eschewing any organizational logic. You push through more instrumental filler and arrive at a sparkling gem: &ldquo;The Birthday Party.&rdquo; An alt-country ballad ornamented with banjo and pedal-steel, the song exposes the paranoia and disenchantment Matty Healy holds with his own behavior: &ldquo;I depend/On my friends/To stay clean.&rdquo;</p>
+        <p>The entire album continues in this hit-or-miss fashion, flirting with a jumbled ensemble of pop templates. Some songs make it seem like Matty&rsquo;s trying on styles just for laughs, such as &ldquo;Roadkill,&rdquo; which emulates Pinegrove so closely that I swear the line about a &ldquo;tucked-up erection&rdquo; is throwing shade at them. &ldquo;Me and You Together Song&rdquo;&mdash;a direct injection of jangly dream pop stolen straight from the 90&rsquo;s&mdash;almost looks like the real thing, but is smothered with a sheen of self-aware tackiness. It successfully rides the line between affecting and annoying, but &ldquo;If You&rsquo;re Too Shy (Let Me Know)&rdquo; crosses it. The chugging synths and yearning lyrics of the verses play to the band&rsquo;s strengths, but the chorus hews dangerously close to the melody and mentality of &ldquo;Shut Up and Dance.&rdquo; The fact that I&rsquo;ll still listen to the entire song, even enduring the cheesy saxophone solo, illustrates the overall appeal of <em>Notes</em>. It&rsquo;s sickeningly earnest enough to make you shake your head, but its addictive catchiness will make you smile. This isn't any more apparent than on the satisfying closer &ldquo;Guys," where Matty ditches the dress-up. Stripping down to his pure self, he sings a sweetly unadorned ode to his bandmates: &ldquo;You guys are the best thing that ever happened to me.&rdquo; <em>Notes On a Conditional Form </em>is far from a perfect album, but I guarantee if you take the time to sort through the mess, you&rsquo;ll find something to take home.</p>`,
+      },
+      {
+        id: 'lyra-pramuk-fountain',
+        title: 'Fountain',
+        artist: 'Lyra Pramuk',
+        content: `<b>Best album to listen to with headphones in a dark room while imagining that your ceiling fan is a UFO that will carry you into the great beyond</b>
+        <p>On Fountain, Lyra Pramuk is a master shapeshifter. Her voice is stretched, distorted, and multiplied across 7 tracks with vastly different palettes. &ldquo;Tendril&rdquo; is joyous, floating a soaring melody over buoyant hums, while following track &ldquo;Xeno&rdquo; does a 180 as Lyra takes the form of a horde of locusts, swarming around a dark synth that expands until it blocks out the sun. On &ldquo;Cradle&rdquo;, the drone of a cello draws a fog across Lyra&rsquo;s deeply ruminative singing, untils she breaks forth with a wordless rendition of the English hymn &ldquo;O Waly, Waly&rdquo; that transforms the song into a heavenly hymn. &ldquo;Cradle&rdquo; illustrates what each track on Fountain has in common: the ability to transcend, to swirl and layer fragments of the human voice until they become something greater, something beyond the self.</p>`,
+      },
+      {
+        id: 'julianna-barwick-healing-is-a-miracle',
+        title: 'Healing is a Miracle',
+        artist: 'Julianna Barwick',
+        content: `<b>Best album for a mental vacation</b>
+        <p>Julianna Barwick&rsquo;s ethereal experiments have always been soothing, but <em>Healing is a Miracle </em>checks Barwick into therapy. After dealing with the aftershock of a divorce and years of grueling tours, she recently moved from New York to Los Angeles and started dating someone new. She&rsquo;s expanded her typically insular method of layering vocal loops by inviting collaborators to share space on the album. Sigur R&oacute;s&rsquo;s J&oacute;nsi contributes a delicate croon on &ldquo;In Light,&rdquo; flickers of Mary Lattimore&rsquo;s harp decorate &ldquo;Oh, Memory,&rdquo; and Nosaj Thing winds pensive harmonies around Barwick&rsquo;s on &ldquo;Nod.&rdquo; Best listened to when you need to catch a breath.</p>`,
+      },
+      {
+        id: 'autechre-sign',
+        title: 'SIGN',
+        artist: 'Autechre',
+        content: `<b>Best album to play while trying to fix your computer and having a revelation that this is just like doing surgery on a human, leaving you with teary eyes and a newfound connection with your machine</b>
+        <p>Last summer I became spellbound by Autechre&rsquo;s 1994 landmark album <em>Amber</em>. The record is known for being Autechre&rsquo;s most emotional and melodic work, leading the duo to later dismiss it as &ldquo;cheesy.&rdquo; Yet the album&rsquo;s papery textures and haunting atmosphere quickly won me over, along with the realization that <em>Kid A </em>definitely took influence from songs like &ldquo;Montreal.&rdquo; 26 years after <em>Amber</em>, and Autechre is still on the bleeding edge of electronic experimentation. In some ways, <em>SIGN </em>mimics the approach of <em>Amber</em>: it&rsquo;s less cold-blooded than Autechre&rsquo;s other output, relying instead on capturing fleeting feelings. <em>SIGN </em>functions as a splatter painting, featuring a myriad of metallic synths that leave a mark on the canvas before dissipating. Squint hard enough and you might be able to make out a sketch of a melody, but more than anything else <em>SIGN </em>is about setting the mood.</p>`,
+      },
+      {
+        id: 'eartheater-phoenix-flames-are-dew-upon-my-skin',
+        title: 'Phoenix: Flames Are Dew Upon My Skin',
+        artist: 'Eartheater',
+        content: `<b>Best album to listen to while exploring a foggy forest in the middle of the night</b>
+        <p>Eartheater&rsquo;s new album lurks under the surface. Alexandra&rsquo;s ghostly vocals join a mystical arrangement of baroque strings on a journey down the river Styx, a sheet of smoke hung over the water to obscure any destination. After escaping haunts of metal-infused glitches and computer-mangled voices, the album finds its haven in the centerpiece &ldquo;Volcano.&rdquo; A classically-bent piano pirouettes while a dejected angel laments (&ldquo;I'm still grinding bones to bake my bread/Grinding on a skull to get ahead&rdquo;). The song presents constant sorrow as a comfort, something to count on. As the album&rsquo;s title suggests, <em>Phoenix: Flames Are Dew Upon My Skin </em>shapes pain into something that glistens. When you&rsquo;re floating down a burning landscape, there&rsquo;s only one thing you can do: take in the view.</p>`,
+      },
+      {
+        id: 'phoebe-bridgers-punisher',
+        title: 'Punisher',
+        artist: 'Phoebe Bridgers',
+        content: `<b>Best album to listen to while mimicking the album’s cover</b>
+        <p>Since her first album <em>Stranger in the Alps, </em>Phoebe&rsquo;s been on a constant upwards trajectory. That album gave hints of her greatness as a songwriter as well as a bittersweet hook crafter (&ldquo;Motion Sickness&rdquo;), and her following collaborative projects as boygenius and Better Oblivion Community Center got me even more excited for her next release. <em>Punisher </em>came out into the world a day early, while I was at my family&rsquo;s cabin by the Tennessee River. I waited till everyone had fallen asleep and walked down to our dock that overlooked the water, sitting there and listening to <em>Punisher </em>front to back while watching the river&rsquo;s reflection of the moon. As the album cover suggests, <em>Punisher </em>is best experienced like this - on your own, in the dark, preferably with a vast sky above you.</p>`,
+      },
+      {
+        id: 'moses-sumney-grae-perfume-genius-set-my-heart-on-fire-immediately',
+        title: 'grae/set my heart on fire immediately',
+        artist: 'moses sumney/perfume genius',
+        content: `<b>Twinsies award</b>
+        <p>Two of my most anticipated albums of 2020, <em>Grae </em>and <em>Set My Heart On Fire Immediately </em>are soulmates. Released on the same day, both mine deep within the psyche for a sense of identity, uncovering mind-body disconnects and reckoning with a world not quite ready to do the same. They confront the strangeness of sharing feelings and physicality with other humans while still trying to discover one&rsquo;s own place in life. Both have fine-tuned their distinct styles, dodging genre labels and expanding their sound to match their broadening view of self.</p>`,
+      },
+      {
+        id: 'moses-sumney-grae-perfume-genius-set-my-heart-on-fire-immediately',
+        title: 'grae/set my heart on fire immediately',
+        artist: 'moses sumney/perfume genius',
+        content: `<b>Twinsies award</b>
+        <p>Two of my most anticipated albums of 2020, <em>Grae </em>and <em>Set My Heart On Fire Immediately </em>are soulmates. Released on the same day, both mine deep within the psyche for a sense of identity, uncovering mind-body disconnects and reckoning with a world not quite ready to do the same. They confront the strangeness of sharing feelings and physicality with other humans while still trying to discover one&rsquo;s own place in life. Both have fine-tuned their distinct styles, dodging genre labels and expanding their sound to match their broadening view of self.</p>`,
+      },
+      {
+        id: 'ka-descendents-of-cain',
+        title: 'Grae/Set My Heart On Fire Immediately',
+        artist: 'Moses Sumney/Perfume Genius',
+        content: `<b>Twinsies award</b>
+        <p>Two of my most anticipated albums of 2020, <em>Grae </em>and <em>Set My Heart On Fire Immediately </em>are soulmates. Released on the same day, both mine deep within the psyche for a sense of identity, uncovering mind-body disconnects and reckoning with a world not quite ready to do the same. They confront the strangeness of sharing feelings and physicality with other humans while still trying to discover one&rsquo;s own place in life. Both have fine-tuned their distinct styles, dodging genre labels and expanding their sound to match their broadening view of self.</p>`,
+      },
+      {
+        id: 'r.a.p.-ferraria-purple-moonlight-pages',
+        title: 'Purple Moonlight Pages',
+        artist: 'R.A.P. Ferraria',
+        content: `<b>Best metaphysical beat poetry that makes you feel like an intellectual</b>
+        <p>On <em>Purple Moonlight Pages</em>, R.A.P. Ferraria spits handpicked phrases packed with eloquently enunciated syllables, neatly fitting them into tapestries of spacey beats that put an emphasis on atmosphere. The album sits you in the audience of a dimly lit theatre while Rory takes center stage, mediating on everything from laundry to the merit of art, occupying the space between the tangible and the beyond.</p>`,
+      },
+      {
+        id: 'armand-hammer-shrines',
+        title: 'Shrines',
+        artist: 'Armand Hammer',
+        content: `<b>Best candidate to bring underground rap to the surface of the earth</b>
+        <p><em>Shrines </em>takes the kaleidoscopic approach to sampling typical of underground rap&mdash;almost every producer on the album is unique to one song&mdash;while disillusioned duo billy woods and ELUCID trade hurried verses about the corruption and lies spread by those in charge. Their breakneck pace &agrave; la Run the Jewels is unusual in a scene that typically favors a patient kind of wit over adrenaline-fueled urgency (see: ka, MIKE, Navy Blue), yet Armand Hammer has all of the above in spades, enlisting a star-studded cast of experimental standouts (Earl Sweatshirt, Pink Siifu, R.A.P. Ferraria) to share bars while he catches breath.</p>`,
+      },
+      {
+        id: 'destroyer-have-we-met',
+        title: 'Have We Met',
+        artist: 'Destroyer',
+        content: `<b>Best sophisti-pop album to play while shaking out existential dread</b>
+        <p><em>Have We Met </em>has a special place in my heart because it was the first album I wrote a review on. The lyrics are a little less cryptic than a typical Destroyer album&mdash;thematically centered around the idea of death&mdash;yet it features some of the catchiest pop songs in his catalog (&ldquo;The Raven&rdquo;, &ldquo;The Man in Black&rsquo;s Blues&rdquo;). It&rsquo;s telling that Bejar released this album in January, before 2020 had grown its fangs. One of the best lines on the album has only become all the more salient: &ldquo;Just look at the world around you/Actually, no, don't look.&rdquo;</p>`,
+      },
+      {
+        id: 'navy-blue-songs-of-sage:-post-panic!',
+        title: 'Songs of Sage: Post Panic!',
+        artist: 'Navy Blue',
+        content: `<b>Best candidate to bring underground rap to the surface of the earth</b>
+        <p>Sage Elsesser&rsquo;s mournful project <em>Songs of Sage: Post Panic! </em>opens in a state of peril. Powerful chords crash down like waves in slow motion. Sage reckons with an understood sense of grief: &ldquo;I often rеminisce, what spirit guides a calm regrеt?/Look myself in the mirror, start tearing up as I reflect.&rdquo; But like the guiding trill of flute that floats throughout the song, he holds onto hope, keeping his eyes on the distant journey ahead of him. Following track &ldquo;Tired&rdquo; looks outside of Sage to see a world strife with unjust killings, pushing him even further below the surface. He spends the rest of the album trying to reach shore, and finds it on &ldquo;Alignment.&rdquo; Even as he steps on dry land, he knows the tides could change at any moment, pulling him back out to sea.</p>`,
+      },
+      {
+        id: 'dogleg-melee',
+        title: 'Melee',
+        artist: 'Dogleg',
+        content: `<b>Best album to remind me of the chaotic and sudden end to my college experience</b>
+        <p>I see red every time I play this record. It released the same week covid first wreaked havoc in the U.S., and its furiously fast pace kept stride with the speed of the suddenly changing world. Listening to songs like &ldquo;Bueno&rdquo; break down and then build back up with walls of pure angst was so cathartic that I can&rsquo;t listen to the album now without returning to my cooped up apartment full of people preparing to flee the virus.</p>`,
+      },
+      
     ],
     createdAt: 1624082838,
   },
